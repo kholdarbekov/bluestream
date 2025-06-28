b5fa67819f0b2d937191e80b9ee1afdb6bb40fa2
Umar Xoldarbekov <umar@MacBook-Pro.local>
2025-06-28 18:25:04 +0500

    fix start-gunicorn.sh

--- app/scripts/start-gunicorn.sh
+++ app/scripts/start-gunicorn.sh
@@ -1,15 +1,7 @@
 #! /usr/bin/env sh
 
 set -e
-
-if [ -f /app/app/main.py ]; then
-    DEFAULT_MODULE_NAME=app.main
-elif [ -f /app/main.py ]; then
-    DEFAULT_MODULE_NAME=main
-fi
-MODULE_NAME=${MODULE_NAME:-$DEFAULT_MODULE_NAME}
-VARIABLE_NAME=${VARIABLE_NAME:-app}
-export APP_MODULE=${APP_MODULE:-"$MODULE_NAME:$VARIABLE_NAME"}
+APP_MODULE='/app/app'
 
 cpu_count=$(grep -c ^processor /proc/cpuinfo)
 #workers=$((1 * $cpu_count))

2a71ba01823938bbda83db645694bf10909d20ad
Umar Xoldarbekov <umar@MacBook-Pro.local>
2025-07-02 22:48:21 +0500

    updates

--- app/__init__.py
+++ app/__init__.py
@@ -1,36 +1,20 @@
 import os
 
 from flask import Flask
+from config import Config
+from app.extensions import db as db_extension
 
 
-def create_app(test_config=None):
+def create_app(config_class=Config):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'core.sqlite'),
-    )
+    app.config.from_object(config_class)
 
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
+    db_extension.init_app(app)
 
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
-
-    # a simple page that says hello
-    @app.route('/hello')
-    def hello():
-        return 'Hello, World!'
-    
-    from . import db, auth, landing
+    from . import main, db, auth, landing
     db.init_app(app)
+    app.register_blueprint(main.bp)
     app.register_blueprint(auth.bp)
     app.register_blueprint(landing.bp)
 
---
+++ app/models/product.py
@@ -0,0 +1,9 @@
+from app.extensions import db
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(150))
+    content = db.Column(db.Text)
+
+    def __repr__(self):
+        return f'<Product "{self.title}">'

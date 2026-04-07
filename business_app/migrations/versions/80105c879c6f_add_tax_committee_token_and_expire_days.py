f7fe376377fbcfeede2907515650608114b9c61c
Umar Xoldarbekov <umar@MacBook-Pro.local>
2026-04-07 12:43:42 +0500

    fix migration

--- business_app/migrations/versions/80105c879c6f_add_tax_committee_token_and_expire_days.py
+++ business_app/migrations/versions/80105c879c6f_add_tax_committee_token_and_expire_days.py
@@ -1,6 +1,6 @@
 """add tax committee api token table and product expire_days
 
-Revision ID: a1b2c3d4e5f6
+Revision ID: 80105c879c6f
 Revises: f66d4fcce111
 Create Date: 2026-04-06 12:00:00.000000
 
@@ -9,7 +9,7 @@ from alembic import op
 import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
-revision = 'a1b2c3d4e5f6'
+revision = '80105c879c6f'
 down_revision = 'f66d4fcce111'
 branch_labels = None
 depends_on = None

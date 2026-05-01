ec44aa8ce93702176c9e1227a92927097b0ee7e6
Umar <international-2014@mail.ru>
2026-05-01 06:57:14 +0000

    Fix frontend blueprint route import

--- business_app/frontend/__init__.py
+++ business_app/frontend/__init__.py
@@ -6,3 +6,5 @@ Handles all frontend routes and UI rendering
 from flask import Blueprint
 
 frontend_bp = Blueprint("frontend", __name__, template_folder="../templates", static_folder="../static")
+
+from . import routes  # noqa: E402,F401  (import after blueprint definition to register routes)
